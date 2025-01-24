2f70c837e44eafbcd41f1ec30353a3f59d27eec5
yashweblife <yashweblife20@gmail.com>
2025-01-24 01:20:36 -0800

    add firebase hooks

---
+++ hooks/useFireStore.ts
@@ -0,0 +1,22 @@
+import { generateID } from "@/lib/utils";
+import { doc, setDoc } from "firebase/firestore";
+import useFirebase from "./useFirebase";
+
+export default function useFireStore() {
+    const {db} = useFirebase()
+    const createNewUserStore = async (id:string, name: string) => {
+        try {
+            const newUserData = {
+                pid: generateID(),
+                name:name,
+                contacts:[],
+                rooms:[],
+            }
+            await setDoc(doc(db, "users", id), newUserData);
+        } catch(error) {
+            console.log(error)
+        }
+    }
+    const createChatRoom = async (id:string, email: string, password: string) => {}
+    return {createNewUserStore, createChatRoom}
+}
---
+++ hooks/useFirebaseAuth.ts
@@ -0,0 +1,45 @@
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { useEffect, useState } from "react";
+import useFirebase from "./useFirebase";
+import useFireStore from "./useFireStore";
+
+export default function useFirebaseAuth(){
+    const {auth} = useFirebase()
+    const {createNewUserStore} = useFireStore()
+    const [user, setUser] = useState(auth.currentUser);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+            }
+        });
+    
+        return () => unsubscribe();
+    }, [auth])
+    const loginUser = async (email: string, password: string) => {
+        try {
+            const user = await signInWithEmailAndPassword(auth, email, password);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    const createNewUser = async (email: string,name:string, password: string) => {
+        try {
+            const user = await createUserWithEmailAndPassword(auth, email, password);
+            await createNewUserStore(user.user.uid, name)
+            setUser(user.user);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    const logoutUser = async () => {
+        try {
+            await auth.signOut();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
+    return {loginUser, createNewUser, logoutUser, user}
+}
