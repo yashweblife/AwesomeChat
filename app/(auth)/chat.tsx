f5426140253b9c02ad34b988246b6742c62200cd
yashweblife <yashweblife20@gmail.com>
2025-01-23 23:23:18 -0800

    reorder chat interface

--- app/(auth)/chat.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from "react-native";
-import { Text } from "react-native-paper";
-export default function ChatPage() {
-    return (
-        <View>
-            <Text>ChatPage</Text>
-        </View>
-    );
-}
