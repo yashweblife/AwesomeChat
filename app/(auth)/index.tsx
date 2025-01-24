e68320da2d1dd56ebb3b9e1ef5f1256619491f53
yashweblife <yashweblife20@gmail.com>
2025-01-23 23:23:29 -0800

    add dashboard

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,11 +1,41 @@
-import { Stack } from "expo-router";
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import BasicView from "@/components/Shared/BasicView";
+import useContactsList from "@/hooks/useContactsList";
+import { router, Stack } from "expo-router";
+import { Pressable, ScrollView, View } from "react-native";
+import { Appbar, Text } from "react-native-paper";
 export default function DashboardPage() {
+    const { contacts, setSelectedContact } = useContactsList();
+    const handleSelectContact = (id:string) => {
+        router.push(`/chat/${id}`)
+    }
     return (
-        <View>
+        <BasicView>
             <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Text>DashboardPage</Text>
-        </View>
+            <Appbar.Header>
+                <Appbar.Content title="Dashboard" />
+                <Appbar.Action icon="menu" onPress={() => { }} />
+            </Appbar.Header>
+            <ScrollView>
+                <View style={{
+                    flex: 1,
+                    gap: 10,
+                    padding: 20
+                }}>
+                    {contacts.map((contact) => (
+                        <Pressable
+                            key={contact.id}
+                            style={{
+                                padding: 20,
+                                boxShadow: "0 0 0.1em 0.1em rgba(255,255,255, 0.2)",
+                                borderRadius: 10,
+                            }}
+                            onPress={() => handleSelectContact(contact.id)}
+                        >
+                            <Text>{contact.name}</Text>
+                        </Pressable>
+                    ))}
+                </View>
+            </ScrollView>
+        </BasicView>
     );
 }
