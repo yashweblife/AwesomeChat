c389afd7355e565a7cbfb63085d1d71c2497868e
yashweblife <yashweblife20@gmail.com>
2025-01-24 01:20:11 -0800

    fix

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -4,12 +4,13 @@ import { router, Stack } from "expo-router";
 import { Pressable, ScrollView, View } from "react-native";
 import { Appbar, Text } from "react-native-paper";
 export default function DashboardPage() {
-    const { contacts, setSelectedContact } = useContactsList();
+    const { contacts } = useContactsList();
     const handleSelectContact = (id:string) => {
         router.push(`/chat/${id}`)
     }
     return (
         <BasicView>
+            
             <Stack.Screen name="index" options={{ headerShown: false }} />
             <Appbar.Header>
                 <Appbar.Content title="Dashboard" />
