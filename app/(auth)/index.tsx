a06b47cf000f931ab58d255413a0b59c8de93ac4
yashweblife <yashweblife20@gmail.com>
2025-01-24 18:46:00 -0800

    add modals and menus

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -3,10 +3,18 @@ import useContactsList from "@/hooks/useContactsList";
 import { router, Stack } from "expo-router";
 import { useState } from "react";
 import { Pressable, ScrollView, View } from "react-native";
-import { Appbar, FAB, Portal, Text } from "react-native-paper";
+import { Appbar, Button, Card, FAB, Menu, Modal, Portal, Text, TextInput } from "react-native-paper";
 
 export function AddFAB() {
     const [open, setOpen] = useState(false);
+    const [newRoomModal, setNewRoomModal] = useState(false);
+    const [newContactModal, setNewContactModal] = useState(false);
+    const openNewRoomModal = () => { setNewRoomModal(true) }
+    const closeNewRoomModal = () => { setNewRoomModal(false) }
+    const openNewContactModal = () => { setNewContactModal(true) }
+    const closeNewContactModal = () => { setNewContactModal(false) }
+    const handleNewRoomAdd = () => { closeNewRoomModal() }
+    const handleNewContactAdd = () => { closeNewContactModal() }
     return (
         <Portal>
             <FAB.Group
@@ -20,21 +28,84 @@ export function AddFAB() {
                     bottom: 0
                 }}
                 icon="plus"
-                onPress={() => { }}
                 actions={[
                     {
                         icon: "account-plus",
                         label: "New Contact",
-                        onPress: () => { }
+                        onPress: openNewContactModal
                     },
                     {
                         icon: "message-text",
                         label: "New Room",
-                        onPress: () => { }
+                        onPress: openNewRoomModal
                     }
                 ]}
             />
+            <Modal
+                visible={newRoomModal}
+                onDismiss={closeNewRoomModal}
+                contentContainerStyle={{
+                    padding: 20,
+                }}
+            >
+                <Card>
+                    <Card.Title title="New Room" />
+                    <Card.Content>
+                        <TextInput mode="outlined" label="Room Name" />
+                    </Card.Content>
+                    <Card.Actions>
+                        <Button mode="outlined" onPress={closeNewRoomModal}>Discard</Button>
+                        <Button mode="contained" onPress={handleNewRoomAdd}>Add</Button>
+                    </Card.Actions>
+                </Card>
+            </Modal>
+            <Modal
+                visible={newContactModal}
+                onDismiss={closeNewContactModal}
+                contentContainerStyle={{
+                    padding: 20,
+                }}
+            >
+                <Card>
+                    <Card.Title title="New Contact" />
+                    <Card.Content>
+                        <TextInput mode="outlined" label="Contact Name" />
+                        <TextInput mode="outlined" label="Contact PID" />
+                    </Card.Content>
+                    <Card.Actions>
+                        <Button mode="outlined" onPress={closeNewContactModal}>Discard</Button>
+                        <Button mode="contained" onPress={handleNewContactAdd}>Add</Button>
+                    </Card.Actions>
+                </Card>
+            </Modal>
         </Portal>
+    )
+}
+export function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const openMenu = () => { setMenuOpen(true) }
+    const closeMenu = () => { setMenuOpen(false) }
+    const settingsButton = () => {
+        closeMenu();
+        router.push("/settings")
+    }
+    const profileButton = () => {
+        closeMenu();
+        router.push("/profile")
+    }
+    return (
+        <Appbar.Header>
+            <Appbar.Content title="Dashboard" />
+
+            <Menu
+                visible={menuOpen}
+                onDismiss={closeMenu}
+                anchor={<Appbar.Action icon="dots-vertical" onPress={openMenu} />}
+            >
+                <Menu.Item title="Settings" onPress={settingsButton} />
+                <Menu.Item title="Profile" onPress={profileButton} />
+            </Menu>
+        </Appbar.Header>
     )
 }
 export default function DashboardPage() {
@@ -45,10 +116,7 @@ export default function DashboardPage() {
     return (
         <BasicView>
             <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Appbar.Header>
-                <Appbar.Content title="Dashboard" />
-                <Appbar.Action icon="menu" onPress={() => { }} />
-            </Appbar.Header>
+            <Header />
             <ScrollView>
                 <View style={{
                     flex: 1,
